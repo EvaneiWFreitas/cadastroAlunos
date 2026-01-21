c68b61e29880307826888496a747f6cfc20eec2a
prwilliam <nwillian533@gmail.com>
2026-01-21 15:35:05 -0300

    criando as tabelas do banco de dados
    Criando as tabelas do banco de dados do referido projeto

--- criarBD.py
+++ criarBD.py
@@ -13,12 +13,34 @@ except sqlite3.Error as e:
 try:
     with con:
         cur = con.cursor()
-        cur.execute(
-            
-        )     
-        
-         
+        cur.execute(""" CREATE TABLE IF NOT EXISTS cursos(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          nome TEXT,
+          duracao TEXT,
+          preco REAL      
+        )""") 
+
+        print("Tabela Cursos criada com sucesso!")    
+                
 except sqlite3.Error as e:
     print("Erro ao criar a tabela cursos:", e)
+
+
+#Criando Tabela de Turmas
+try:
+    with con:
+        cur = con.cursor()
+        cur.execute(""" CREATE TABLE IF NOT EXISTS turmas(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          nome TEXT,
+          curso_nome TEXT,
+          data_inicio DATE,
+          FOREIGN KEY(curso_nome) REFERENCES cursos(nome) ON UPDATE CASCADE ON DELETE CASCADE     
+        )""") 
+
+        print("Tabela Turmas criada com sucesso!")    
+                
+except sqlite3.Error as e:
+    print("Erro ao criar a tabela turmas:", e)
  
     
