0476eda375935324a2e97545b19ed58ff7965fce
prwilliam <nwillian533@gmail.com>
2026-01-22 08:44:47 -0300

    Criando uma Função para ver os cursos - READ
    CRUD é um acrônimo para Create (Criar), Read (Ler), Update (Atualizar) e Delete (Deletar), Criando Aa Segunda Função para ver Todos os cursos - READ

--- view.py
+++ view.py
@@ -3,7 +3,7 @@ import sqlite3 as lite
 
 #Criando conexao
 try:
-    con = sqlite3.connect('cadastro_alunos.db')
+    con = lite.connect('cadastro_alunos.db')
     print('Conexao com o banco de dados realizada com sucesso...')
 except sqlite3.Error as e:
     print("Erro ao tentar conectar com o banco de dados:", e)
@@ -12,7 +12,31 @@ except sqlite3.Error as e:
 # Tabela de Cursos---------------------------------------------------
 
 # Criando a Função criar cursos(CREATE - INSERIR)
-def criar_cursos(i)
+def criar_cursos(i):
+	with con:
+		cur = con.cursor()
+		query = "INSERT INTO cursos(nome, duracao, preco) VALUES (?,?,?)"
+		cur.execute(query,i)
+
+#criar_cursos(['Python','Duas Semanas',50])
+
+
+#Ver Todos os Cursos(READ - LER OU SELECIONAR TODOS OS CURSOS)
+#Criando uma Função para ver os cursos
+def ver_cursos():
+	lista = []
+	with con:
+		cur = con.cursor()
+		cur.execute('SELECT * FROM cursos')
+		linha = cur.fetchall()
+
+		for i in linha:
+			lista.append(i)
+
+	return lista
+
+print(ver_cursos())
+
 
 
 
